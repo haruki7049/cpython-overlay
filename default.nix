940b2832481e60b858e21bded02a9627ba505237
haruki7049 <tontonkirikiri@gmail.com>
2024-04-30 21:41:01 +0900

    chore: Init default.nix

---
+++ default.nix
@@ -0,0 +1,15 @@
+self: super: {
+  cpython = {
+    "3.7.3" = super.stdenv.mkDerivation rec {
+      pname = "cpython";
+      version = "3.7.3";
+
+      src = super.fetchFromGitHub {
+        owner = "python";
+        repo = pname;
+        rev = "v${version}"
+        hash = "";
+      };
+    };
+  };
+}
