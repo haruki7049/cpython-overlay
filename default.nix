1bab58f1668ef38c6456f9d8c84a61dabc9e185e
haruki7049 <tontonkirikiri@gmail.com>
2024-05-13 22:19:45 +0900

    chore: Edit default.nix

--- default.nix
+++ default.nix
@@ -10,6 +10,24 @@ self: super: {
         rev = "v${version}";
         hash = "sha256-DMz2VdmnNEQIO4ALBP+KBccP3DxI/z+mKQMaReRg070=";
       };
+
+      buildPhase = ''
+        ./configure --prefix=$out
+        make
+
+        # Run the tests
+        make test
+      '';
+
+      installPhase = ''
+        make install
+      '';
+
+      meta = with super.stdenv.lib; {
+        description = "Python programming language";
+        license = licenses.psfl;
+        platforms = platforms.all;
+      };
     };
   };
 }
