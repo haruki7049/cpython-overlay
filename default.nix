fc1fa08f689edca20b4b1e5ae3802126e34c121b
haruki7049 <tontonkirikiri@gmail.com>
2024-05-14 06:17:06 +0900

    chore: nix fmt

--- default.nix
+++ default.nix
@@ -1,116 +1,129 @@
 self: super: {
-  cpython =
-    let
-      pkgs = super.pkgs;
-      lib = super.lib;
-      fetchFromGitHub = super.fetchFromGitHub;
-      stdenv = pkgs.stdenv;
-      optionals = lib.optionals;
-      pkg-config = pkgs.pkg-config;
-      autoreconfHook = pkgs.autoreconfHook;
-      autoconf-archive = pkgs.autoconf-archive;
-      nukeReferences = pkgs.nukeReferences;
-      zlib = pkgs.zlib;
-      glibc = pkgs.glibc;
-      gdbm = pkgs.gdbm;
-      sqlite = pkgs.sqlite;
-      bzip2 = pkgs.bzip2;
-      expat = pkgs.expat;
-      ncurses = pkgs.ncurses;
-      readline = pkgs.readline;
-      xz = pkgs.xz;
-      libffi = pkgs.libffi;
-      libxcrypt = pkgs.libxcrypt;
-      tcl = pkgs.tcl;
-      tk = pkgs.tk;
-      libX11 = pkgs.xorg.libX11;
-      xorgproto = pkgs.xorg.xorgproto;
-      bluez = pkgs.bluez;
-      tzdata = pkgs.tzdata;
-      openssl-1-0-2 = stdenv.mkDerivation {
-        pname = "openssl";
-        version = "1.0.2";
+  cpython = let
+    pkgs = super.pkgs;
+    lib = super.lib;
+    fetchFromGitHub = super.fetchFromGitHub;
+    stdenv = pkgs.stdenv;
+    optionals = lib.optionals;
+    pkg-config = pkgs.pkg-config;
+    autoreconfHook = pkgs.autoreconfHook;
+    autoconf-archive = pkgs.autoconf-archive;
+    nukeReferences = pkgs.nukeReferences;
+    zlib = pkgs.zlib;
+    glibc = pkgs.glibc;
+    gdbm = pkgs.gdbm;
+    sqlite = pkgs.sqlite;
+    bzip2 = pkgs.bzip2;
+    expat = pkgs.expat;
+    ncurses = pkgs.ncurses;
+    readline = pkgs.readline;
+    xz = pkgs.xz;
+    libffi = pkgs.libffi;
+    libxcrypt = pkgs.libxcrypt;
+    tcl = pkgs.tcl;
+    tk = pkgs.tk;
+    libX11 = pkgs.xorg.libX11;
+    xorgproto = pkgs.xorg.xorgproto;
+    bluez = pkgs.bluez;
+    tzdata = pkgs.tzdata;
+    openssl-1-0-2 = stdenv.mkDerivation {
+      pname = "openssl";
+      version = "1.0.2";
 
-        src = fetchFromGitHub {
-          owner = "openssl";
-          repo = "openssl";
-          rev = "OpenSSL_1_0_2";
-          sha256 = "sha256-so3qv87ph1mA3LMi4zOQ2hOlo9DApbuQc33GSWnBl/E=";
-        };
-
-        buildInputs = with pkgs; [ perl ];
-
-        buildPhase = ''
-          ./config --prefix=$out
-          make
-        '';
-
-        installPhase = ''
-          make install
-        '';
+      src = fetchFromGitHub {
+        owner = "openssl";
+        repo = "openssl";
+        rev = "OpenSSL_1_0_2";
+        sha256 = "sha256-so3qv87ph1mA3LMi4zOQ2hOlo9DApbuQc33GSWnBl/E=";
       };
-    in
-    {
-      "3.7.3" = stdenv.mkDerivation rec {
-        pname = "cpython";
-        version = "3.7.3";
 
-        src = fetchFromGitHub {
-          owner = "python";
-          repo = pname;
-          rev = "v${version}";
-          hash = "sha256-DMz2VdmnNEQIO4ALBP+KBccP3DxI/z+mKQMaReRg070=";
-        };
+      buildInputs = with pkgs; [ perl ];
 
-        buildInputs = [ glibc openssl-1-0-2 zlib gdbm sqlite bzip2 expat ncurses readline xz libffi libxcrypt tcl tk libX11 xorgproto bluez tzdata ];
+      buildPhase = ''
+        ./config --prefix=$out
+        make
+      '';
 
-        nativeBuildInputs = optionals (!stdenv.isDarwin)
-          [ pkg-config autoreconfHook autoconf-archive ] ++ [
-          nukeReferences
-        ] ++ optionals (stdenv.cc.isClang && (!stdenv.hostPlatform.useAndroidPrebuilt or false)) [
-          stdenv.cc.cc.libllvm.out
-        ] ++ [
-          openssl-1-0-2
-        ];
+      installPhase = ''
+        make install
+      '';
+    };
+  in {
+    "3.7.3" = stdenv.mkDerivation rec {
+      pname = "cpython";
+      version = "3.7.3";
 
-        LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
-        LDFLAGS = [
-          "-L${glibc}"
-          "-L${zlib}"
-        ];
+      src = fetchFromGitHub {
+        owner = "python";
+        repo = pname;
+        rev = "v${version}";
+        hash = "sha256-DMz2VdmnNEQIO4ALBP+KBccP3DxI/z+mKQMaReRg070=";
+      };
 
-        enableParallelBuilding = true;
+      buildInputs = [
+        glibc
+        openssl-1-0-2
+        zlib
+        gdbm
+        sqlite
+        bzip2
+        expat
+        ncurses
+        readline
+        xz
+        libffi
+        libxcrypt
+        tcl
+        tk
+        libX11
+        xorgproto
+        bluez
+        tzdata
+      ];
 
-        #patches = [
-        #  ./cpython-3.7.3/static-openssl.patch
-        #];
+      nativeBuildInputs = optionals (!stdenv.isDarwin) [
+        pkg-config
+        autoreconfHook
+        autoconf-archive
+      ] ++ [ nukeReferences ] ++ optionals (stdenv.cc.isClang
+        && (!stdenv.hostPlatform.useAndroidPrebuilt or false))
+        [ stdenv.cc.cc.libllvm.out ] ++ [ openssl-1-0-2 ];
 
-        buildPhase = ''
-          ./configure \
-            --prefix=$out \
-            --enable-optimizations \
-            --with-lto \
-            --with-openssl=${openssl-1-0-2} \
-            --with-openssl-rpath=$out/lib \
-            --with-ssl-default-suites=openssl \
-            --with-zlib=${zlib}
-            #--with-libc=${stdenv.cc.libc}
+      LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+      LDFLAGS = [ "-L${glibc}" "-L${zlib}" ];
 
-          #make -s
+      enableParallelBuilding = true;
 
-          # Run the tests
-          #make test
-        '';
+      #patches = [
+      #  ./cpython-3.7.3/static-openssl.patch
+      #];
 
-        installPhase = ''
-          make install
-        '';
+      buildPhase = ''
+        ./configure \
+          --prefix=$out \
+          --enable-optimizations \
+          --with-lto \
+          --with-openssl=${openssl-1-0-2} \
+          --with-openssl-rpath=$out/lib \
+          --with-ssl-default-suites=openssl \
+          --with-zlib=${zlib}
+          #--with-libc=${stdenv.cc.libc}
 
-        meta = with lib; {
-          description = "Python programming language";
-          license = licenses.psfl;
-          platforms = platforms.linux;
-        };
+        #make -s
+
+        # Run the tests
+        #make test
+      '';
+
+      installPhase = ''
+        make install
+      '';
+
+      meta = with lib; {
+        description = "Python programming language";
+        license = licenses.psfl;
+        platforms = platforms.linux;
       };
     };
+  };
 }
